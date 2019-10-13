633edf64c9328a64c575b1c74293f6879278356a
Ginkgoch <ginkgoch@outlook.com>
2019-10-13 22:04:22 +0800

    266: Sign up support.

--- src/routers/UsersRouter.ts
+++ src/routers/UsersRouter.ts
@@ -2,15 +2,16 @@ import Router from "koa-router";
 import bodyParser from "koa-body";
 import { Utils } from "../shared";
 import { Repositories } from "../repositories/Repositories";
+import { UserModel } from "../models";
 
 const router = new Router();
-router.post('register user', '/signup', bodyParser(), async ctx => {
-    const requestingUser = Utils.parseRequestBody(ctx);
+router.post('register user', '/users/signup', bodyParser(), async ctx => {
+    const requestingUser = Utils.parseRequestBody(ctx) as UserModel;
     if (!requestingUser.name || !requestingUser.email || !requestingUser.password) {
         ctx.throw(400, 'Failed to create new user. User model must include name, email and password.');
     }
 
-    let existingUser = await Repositories.users.getUserBy(new Map([['name', requestingUser.name], 'email', requestingUser.email]), 'OR');
+    let existingUser = await Repositories.users.getUserBy(new Map([['name', requestingUser.name], ['email', requestingUser.email]]), 'OR');
     if (existingUser !== undefined) {
         let failedReason = 'Failed to create new user.';
         if (existingUser.name === requestingUser.name) {
@@ -22,6 +23,11 @@ router.post('register user', '/signup', bodyParser(), async ctx => {
 
         ctx.throw(409, failedReason);
     }
+
+    const result = await Repositories.users.insert(requestingUser);
+    requestingUser.id = result.lastID;
+    requestingUser.password = ''.padStart(requestingUser.password.length, '*');
+    Utils.json(requestingUser, ctx);
 });
 
 export const UsersRouter = router;
